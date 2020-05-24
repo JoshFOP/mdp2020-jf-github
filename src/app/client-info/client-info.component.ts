807adfe936a5ba8784b6862e35b020d234af8124
JoshFOP <joshuafiorenza2003@gmail.com>
2020-05-24 19:09:55 +1000

    3 files modified A clients jobs can be viewed on their client info page.

--- src/app/client-info/client-info.component.ts
+++ src/app/client-info/client-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClientsService } from '../services/clients.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { JobsService } from '../services/jobs.service';
 
 @Component({
   selector: 'app-client-info',
@@ -13,7 +14,8 @@ export class ClientInfoComponent implements OnInit {
 constructor(
     private route: ActivatedRoute,
     private fb: FormBuilder,
-    private cs: ClientsService) { }
+    private cs: ClientsService,
+    private js: JobsService) { }
 
   // Make my 'peopleForm' a FormGroup
   clientsForm: FormGroup;
@@ -22,12 +24,14 @@ constructor(
   id: number;
   private sub: any;
   //people array
-  clients: any
+  clients: any;
+  jobs: any;
 
   ngOnInit() {
     // Call the PeopleService Method 'getPeopleArray'
     // returns all the people data
     this.clients = this.cs.getClient();
+    this.jobs = this.js.getJob()
 
     // This code graps the "id" from the URL
     this.sub = this.route.params.subscribe(params => {
